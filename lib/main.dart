d4a78a4d0bee274f8567c85411853f13eb02c220
Sai Praneth Raju K <sai.praneth10@gmail.com>
2022-12-26 14:15:04 -0800

    Added Firebase Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:vonette_web/app_theme/theme_manager.dart';
-import 'package:vonette_web/constants.dart';
 import 'package:vonette_web/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:scaled_app/scaled_app.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   return runApp(ChangeNotifierProvider<ThemeNotifier>(
